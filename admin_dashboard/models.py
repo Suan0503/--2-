f2d0ee2a78991d4459d7e30d082ba1b651453394
Suan0503 <a12365478957@gmail.com>
2025-06-05 17:34:37 +0800

    Update models.py

--- admin_dashboard/models.py
+++ admin_dashboard/models.py
@@ -1,1 +1,8 @@
+from flask_sqlalchemy import SQLAlchemy
 
+db = SQLAlchemy()
+
+class Whitelist(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    phone = db.Column(db.String(20), unique=True)
+    name = db.Column(db.String(80))
