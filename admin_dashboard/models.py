5bc759e8df3cdb066bfae65c2cae024b4fbb8030
Suan0503 <a12365478957@gmail.com>
2025-06-05 18:35:36 +0800

    Update models.py

--- admin_dashboard/models.py
+++ admin_dashboard/models.py
@@ -1,8 +1,20 @@
-from flask_sqlalchemy import SQLAlchemy
-
-db = SQLAlchemy()
-
-class Whitelist(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    phone = db.Column(db.String(20), unique=True)
-    name = db.Column(db.String(80))
+<!DOCTYPE html>
+<html>
+<head><title>登入</title></head>
+<body>
+    <form method="POST">
+        <input type="text" name="username" placeholder="帳號" required>
+        <input type="password" name="password" placeholder="密碼" required>
+        <button type="submit">登入</button>
+    </form>
+    {% with messages = get_flashed_messages() %}
+      {% if messages %}
+        <ul>
+          {% for msg in messages %}
+            <li>{{ msg }}</li>
+          {% endfor %}
+        </ul>
+      {% endif %}
+    {% endwith %}
+</body>
+</html>
